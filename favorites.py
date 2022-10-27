149288a6295d78362bcb0bc2fb45155229be6058
domiscroissant <114240319+domiscroissant@users.noreply.github.com>
2022-10-27 08:07:14 -0600

    Create favorites.py

---
+++ favorites.py
@@ -0,0 +1,11 @@
+cookies = int(input("how many cookies do you have?"))
+if cookies <3:
+    print("you don't have enough cookies")
+elif cookies <10:
+    print("you have good amount")
+else:
+    print("you have too many")
+   
+  
+    
+        
