6cabad2d8e5a9c54477c3d73189f0bae8b495172
domiscroissant <114240319+domiscroissant@users.noreply.github.com>
2022-10-27 10:00:35 -0600

    Update favorites.py

--- favorites.py
+++ favorites.py
@@ -1,3 +1,4 @@
+1.
 cookies = int(input("how many cookies do you have?"))
 if cookies <3:
     print("you don't have enough cookies")
@@ -5,7 +6,42 @@ elif cookies <10:
     print("you have good amount")
 else:
     print("you have too many")
-   
-  
+2.
+side = input("are you a jedi master or sith lord?")
+if side == 'jedi':
+    print("you get a green light saber")
+elif side == 'sith':
+    print("you get a red light saber")
+else:
+    print("you get breadstick")
+3.
+print("loop #1:")
+for i in range(4, 41, 2):
+    print(i, end =" ")
+4.
+i = 100
+while i < 50:
+    print(i)
+    i += 10
+5.
+quit=False
+while quit == False:
+    choice=input("knock knock, who's there banana")
+    if choice == 'orange':
+        print("yes")
+    else:
+        print("no")
+        quit=True
+        
+print("orange you glad I didn't say banana")
+6.
+def multiple (x,y,z):
+    if x<y<z:
+        return x*y*z
+    else:
+        return x/y/z
+print(multiple(2,3,4))
+7.
+I think it's better if I try this one again next friday, sorry
     
         
